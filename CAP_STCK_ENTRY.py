2c671733f68a34f6d4fc34ddd6dedcc92a1765cb
Sourcery AI <>
2023-06-06 10:28:06 +0000

    'Refactored by Sourcery'

--- CAP_STCK_ENTRY.py
+++ CAP_STCK_ENTRY.py
@@ -14,25 +14,27 @@ def stock_entry_option():
     print ('Please note that the program supports Stocks listed in S&P500 as of now')
     r = raw_input ('Select F to choose a file of tickers\OR\Select M for manual input of tickers: ')
     StockTickerArray = []
-        
-    if r != 'F' and r != 'M':
+
+    if r not in ['F', 'M']:
         raise ValueError("\nOops! wrong entry\Enter only F or M")
-    
+
     if r == 'F':#for upload through file    
         user_input = raw_input("Enter your ticker file: ")
-        assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+        assert os.path.exists(
+            user_input
+        ), f"I did not find the file at, {str(user_input)}"
         #open the file
         xlsx = pd.ExcelFile(user_input)
         S1 = xlsx.parse(0)
         StockTickerArray = (S1['TICKERS'].values.tolist())
-        
+
     else: #to upload manually   
         StockCount = input ('Input the number of stocks in the portfolio: ')
-        S1 = list()
+        S1 = []
         for i in range(1,StockCount+1):
-            StockTicker = raw_input ('Enter Stock Ticker '+str(i)+': ')
+            StockTicker = raw_input(f'Enter Stock Ticker {str(i)}: ')
             StockTickerArray.append(str(StockTicker)) 
-            
+
     #Broad Market Index is entered         
-    StockTickerArray.append('^GSPC')        
+    StockTickerArray.append('^GSPC')
     return StockTickerArray
